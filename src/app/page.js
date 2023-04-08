3783854e2cb1c2c26d2a7f7b14af789540a1f63d
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-09 01:12:42 +0530

    added loading animation

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import logo from "public/logo.jpg";
 import Link from "next/link";
 import axios from 'axios';
 import {AnimatePresence, motion} from "framer-motion";
+// import {Configuration, OpenAIApi} from "openai";
 
 const kanit = Kanit({
     subsets: ["latin"],
@@ -23,21 +24,59 @@ export default function Page() {
     const [articles, setArticles] = useState([]);
     const [isSearched, setIsSearched] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
 
+    // const OPENAI_API_KEY = "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"
+    //
+    // async function getResponse() {
+    //     const options = {
+    //         method: 'POST',
+    //         headers: {
+    //             "Authorization": `Bearer ${OPENAI_API_KEY}`,
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({
+    //             model: "text-davinci-003",
+    //             messages: [{role: "user", content: "How are you"}],
+    //             max_tokens: 64,
+    //         })
+    //     }
+    //
+    //     try {
+    //         const response = await fetch("https://api.openai.com/v1/chat/completions", options)
+    //         const data = await response.json()
+    //         console.log(data)
+    //
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
 
     const getArticles = async (query) => {
-        const response = await axios.get(
-            `https://newsapi.org/v2/everything?q=${query}&sortBy=popularity&page=1&pagesize=10&language=en&apiKey=c727a28d08b84681a92931c904b93a02`
-        );
-        setArticles(response.data.articles);
+        try {
+            const response = await axios.get(
+                `https://newsapi.org/v2/everything?q=${query}&sortBy=popularity&page=1&pagesize=10&language=en&apiKey=c727a28d08b84681a92931c904b93a02`
+            );
+            setArticles(response.data.articles);
+        } catch (error) {
+            setIsError(true);
+        }
     };
 
     async function handleSearch() {
-        const response = await fetch(`/api/factcheck?query=${encodeURIComponent(query)}`);
-        const results = await response.json();
-        setFactChecks(results);
+        setIsLoading(true)
+        setIsError(false)
+        try {
+            const response = await fetch(`/api/factcheck?query=${encodeURIComponent(query)}`);
+            const results = await response.json();
+            setFactChecks(results);
+        } catch (error) {
+            setIsError(true)
+        }
         await getArticles(query)
         setIsSearched(true)
+        setIsLoading(false)
     }
 
     function clearButton() {
@@ -74,6 +113,7 @@ export default function Page() {
     return (
         <main className={`${kanit.className} relative w-full min-h-[100svh] flex flex-col justify-center items-center gap-10 py-16 px-10`}>
 
+            {/*<button onClick={getResponse}>response</button>*/}
             <nav className={"bg-[#f1f1f1] w-full shadow-md fixed top-0 py-2 px-4 flex flex-row justify-between items-center z-10"}>
                 <Link href={"/"}><Image className={"rounded-full w-auto h-10 border-2 border-[#121212]"} width={100} height={100} src={logo} alt={"Fake Or Not Logo"} /></Link>
                 <div className={"flex flex-row gap-4"}>
@@ -90,9 +130,37 @@ export default function Page() {
                 <input required onKeyPress={handleKeyPress} autoFocus={true} placeholder={"Write Your Query Here..."} className={"px-5 border border-black rounded-lg min-w-[38vw] h-[5vh]"} type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
                 <button className={" hover:bg-[#62C370] h-[5vh] px-5 right-0 border-2 border-gray-900 rounded-lg"} onClick={handleSearch}>Search</button>
                 {isSearched && (
-                    <button className={"h-[5vh] px-5 right-0 border-2 border-gray-900 rounded-lg hover:bg-gray-400"} onClick={clearButton}>Clear</button>
+                    <button className={"h-[5vh] px-5 right-0 border-2 border-gray-900 rounded-lg hover:bg-red-400"} onClick={clearButton}>Clear</button>
                 )}
             </motion.div>
+
+            <div>{
+                isLoading ?
+                    <div className="flex justify-center items-center">
+                        <div className="flex space-x-2">
+                            <motion.div
+                                className="h-4 w-4 bg-[#00FFFF] rounded-full"
+                                animate={{ y: [-10, 0, -10] }}
+                                transition={{ duration: 1, repeat: Infinity }}
+                            />
+                            <motion.div
+                                className="h-4 w-4 bg-[#FF00FF] rounded-full"
+                                animate={{ y: [-10, 0, -10] }}
+                                transition={{ duration: 1, delay: 0.2, repeat: Infinity }}
+                            />
+                            <motion.div
+                                className="h-4 w-4 bg-[#FFFF00] rounded-full"
+                                animate={{ y: [-10, 0, -10] }}
+                                transition={{ duration: 1, delay: 0.4, repeat: Infinity }}
+                            />
+                            <motion.div
+                                className="h-4 w-4 bg-black rounded-full"
+                                animate={{ y: [-10, 0, -10] }}
+                                transition={{ duration: 1, delay: 0.6, repeat: Infinity }}
+                            />
+                        </div>
+                    </div> : null
+            }</div>
 
             {isSearched &&
                 // <div className={"flex flex-row min-w-[40vw] justify-between gap-10"}>
@@ -104,12 +172,13 @@ export default function Page() {
                         <div className={"border-b text-lg"}>
                             <h2>Fact Checks</h2>
                         </div>
+                        {isError && <div>No Query found in our database</div>}
                         {/*{factChecks.length === 0 && <li>No results</li>}*/}
                         {factChecks.map((factCheck) => (
                             // eslint-disable-next-line react/jsx-key
                             <AnimatePresence>
                                 <motion.li layoutId={factCheck.claimReview[0].url} initial={{opacity: 0}} animate={{opacity: 1}} transition={{ duration: 0.5 }} className={"border border-black rounded-lg py-4 px-6 flex flex-row justify-between"} key={factCheck.claimReview[0].url}>
-                                    <a target={"_blank"} className={"line-clamp-1"} href={factCheck.claimReview[0].url}>{factCheck.claimReview[0].title}</a>
+                                    <a target={"_blank"} className={"line-clamp-1 hover:text-[#62C370]"} href={factCheck.claimReview[0].url}>{factCheck.claimReview[0].title}</a>
                                     {factCheck.claimReview[0].textualRating === "True" ?
                                         <p className={"ml-10 text-green-500 line-clamp-1"}>{factCheck.claimReview[0].textualRating}</p> :
                                         <p className={"ml-10 text-red-500 line-clamp-1"}>{factCheck.claimReview[0].textualRating}</p>}
@@ -124,11 +193,13 @@ export default function Page() {
                         <div className={"border-b text-lg"}>
                             <h2>Relevant News</h2>
                         </div>
+                        {isError && <div>No Query found in our database</div>}
+                        {/*{articles.length === 0 && <li>No results</li>}*/}
                         {articles.map((article) => (
                             // eslint-disable-next-line react/jsx-key
                             <AnimatePresence>
                                 <motion.div layoutId={article.url} className={"border border-black rounded-lg py-4 px-6 h-max"} initial={{opacity: 0}} animate={{opacity: 1}} transition={{ duration: 0.5 }} >
-                                    <a target={"_blank"} key={article.title} href={article.url}><h2>{article.title}</h2></a>
+                                    <a target={"_blank"} className={"hover:text-[#62C370]"} key={article.title} href={article.url}><h2>{article.title}</h2></a>
                                 </motion.div>
                             </AnimatePresence>
                         ))}
